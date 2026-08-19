f25771d33b8b57e17c0c417cb9c3fe692a17d6a6
jardielmanuel26 <jardielmanuel66@gmail.com>
2026-08-18 21:52:46 -0300

    Prática 16

---
+++ practica-16/js/script.js
@@ -0,0 +1,27 @@
+let miNombre = "Manuel";
+
+miNombre = "Jardiel";
+
+let miApellido = "Yujra";
+
+miApellido = "Salazar";
+
+let miNombreyApellido = "Manuel Yujra";
+
+miNombreyApellido = "Jardiel Salazar"
+
+let miEdad = 22;
+
+miEdad = 12;
+
+const nombreUniversidad = "Universidad Nacional de las Artes";
+
+nombreUniversidad = "Universidad de Buenos Aires" //Al cambiar el valor de la variable "nombreUniversidad", el navegador arroja un error porque al ser declarada con const no se puede reasignar su valor.
+
+
+
+console.log(miNombre);
+console.log(miApellido);
+console.log(miNombreyApellido);
+console.log(miEdad);
+console.log(nombreUniversidad);
