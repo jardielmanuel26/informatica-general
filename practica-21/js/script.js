00f7cecd1b642a65f712f22893f5173482d71907
jardielmanuel26 <jardielmanuel66@gmail.com>
2026-08-27 17:19:53 -0300

    Personalización de la bienvenida.
    -Utilizo un selector de identificador para identificar el mensaje de bienvenida y poderlo personalizar.
    
    -Utilizo los métodos prompt, confirm y los condicionales para interactuar con el usuario.

--- practica-21/js/script.js
+++ practica-21/js/script.js
@@ -1,4 +1,17 @@
 /*Práctica 21 - Javascript*/
 
-/*Pregunta sobre la personalización de la página*/
-let respuesta = confirm("¿Desea personalizar la bienvenida?")
+/*Selecciono el mensaje de bienvenida por su ID*/
+let mensajeBienvenida = document.querySelector("#bienvenida");
+
+/*Pregunta sobre la personalización del mensaje de bienvenida*/
+let respuesta = confirm("¿Desea personalizar la bienvenida?");
+
+/* Uso de la estructura if + else para agregar o no el nombre al mensaje de bienvenida*/
+if (respuesta) {
+    let nombre = prompt("Ingresar nombre", "aquí");
+    if (nombre) {
+        mensajeBienvenida.innerText = "Saludos "  + nombre + "! Te doy la bienvenida a mi página. En esta ocasión se presentarán los pasos para preparar una deliciosa taza de café con espuma.";
+    }
+} else {
+    mensajeBienvenida.innerText = "Saludos! Te doy la bienvenida a mi página. En esta ocasión se presentarán los pasos para preparar una deliciosa taza de café con espuma.";
+};
