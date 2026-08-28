c705f1187c098e9e88b13ee72ee00556274559f6
jardielmanuel26 <jardielmanuel66@gmail.com>
2026-08-28 18:54:47 -0300

    Fin de la práctica 22
    Correcciones y declaración de uso de IA.

--- practica-22/js/script.js
+++ practica-22/js/script.js
@@ -7,16 +7,27 @@ let resultado = document.querySelector("#resultado");
 
 //Defino el evento para el botón de cálculo
 
-botonCalcular.addEventListener("click", function cálculo() {
+botonCalcular.addEventListener("click", function calcularPrecio() {
     let dato1 = document.querySelector("#num1").value;
     let dato2 = document.querySelector("#num2").value;
     
     //Hago el calculo para obtener el precio con descuento
 
-    let montoDescuento = (dato1 * dato2) / 100;
+    let montoDescuento = (Number (dato1) * Number(dato2)) / 100;
     let precioCondescuento = dato1 - montoDescuento;
 
     //Resultado del cálculo
 
     resultado.value = precioCondescuento;
 });
+
+/*Declaración de uso de IA: Se utilizó la herramienta Gemini para pedirle que revise y valide el código de los archivos HTML, CSS y JavaScript, diciéndome que tienen una estructura base funcional
+
+pero presentan errores críticos de lógica, tipos de datos y maquetación CSS que afectan el cálculo y la visualización. De entre las idicaciones que me dio, añadí nuevos estilos al archivo CSS para el formulario y el
+
+botón de cálculo, de esta forma se ve más prolijo. En el arhcivo JavaScript, reemplazé el nombre de la función y en el cálculo del montoDescuento puse a los datos como parámetros de la función Number
+
+para convertirlos en valores numéricos y así evitar errores de concatenación. 
+ 
+ Lo que descarté fue básicamente lo que aún no vimos como el atributo step de input y la función parseFloat.*/
+ 
