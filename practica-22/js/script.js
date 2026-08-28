3a4bc75831d205ac814e9069d4f414e65ed06d99
jardielmanuel26 <jardielmanuel66@gmail.com>
2026-08-28 15:41:26 -0300

    Correciones y sistema de cálculo
    -Correcciones a los archivos CSS y HTML
    -Armado del sistema para realizar cálculos entre dos números en JS.

--- practica-22/js/script.js
+++ practica-22/js/script.js
@@ -1,1 +1,22 @@
 /* Práctica 22 - JavaScript*/
+
+//Capturo los elementos 
+
+let botonCalcular = document.querySelector("#calcular");
+let resultado = document.querySelector("#resultado");
+
+//Defino el evento para el botón de cálculo
+
+botonCalcular.addEventListener("click", function cálculo() {
+    let dato1 = document.querySelector("#num1").value;
+    let dato2 = document.querySelector("#num2").value;
+    
+    //Hago el calculo para obtener el precio con descuento
+
+    let montoDescuento = (dato1 * dato2) / 100;
+    let precioCondescuento = dato1 - montoDescuento;
+
+    //Resultado del cálculo
+
+    resultado.value = precioCondescuento;
+});
