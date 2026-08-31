3ca06ba41a1b19935d6e1c703fdec808785e1567
jardielmanuel26 <jardielmanuel66@gmail.com>
2026-08-31 18:51:49 -0300

    Interactividad y estilos
    -Creación de la función y evento para que el usuario interactúe con el sitio.
    
    -Modifiqué los estilos.

--- practica-24/js/script.js
+++ practica-24/js/script.js
@@ -1,1 +1,19 @@
 /* Práctica 24 - JavaScript*/
+
+//Defino la función para tirar el dado
+function tirarDado() {
+
+//Genero un número entero al azar entre 1 y 6
+let numeroAzar = Math.floor(Math.random() * 6) + 1;
+
+//Selecciono la etiqueta "img" del HTML
+let imgDado = document.querySelector("#imagendado");
+
+//Modifico el atributo "src" de la imagen con el número obtenido
+imgDado.src = numeroAzar + ".png"
+}
+
+//Defino el evento para llevar a cabo toda la función.
+
+let boton = document.querySelector("#dado");
+boton.addEventListener("click", tirarDado);
